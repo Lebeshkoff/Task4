a6ae82e4306c28a288bb9792ee1aa35c4339c4cc
Dmitry Lebeshkov <51168295+Lebeshkoff@users.noreply.github.com>
2021-08-29 23:38:35 +0300

    Добавьте файлы проекта.

---
+++ Task4_Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Task4_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var str = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(str);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:8888/connection/");
+                request.Method = "POST";
+                request.ContentType = "text/*";
+                Stream requestStream = request.GetRequestStream();
+                requestStream.Write(data, 0, data.Length);
+                requestStream.Close();
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                Console.WriteLine(reader.ReadToEnd());
+                response.Close();
+            }
+        }
+    }
+}
---
+++ Task4_test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            
+            listener.Prefixes.Add("http://localhost:8888/connection/");
+            listener.Start();
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                Console.WriteLine(request.HttpMethod + " " + request.UserHostName + " " + request.UserAgent);
+                Stream requestStream = request.InputStream;
+                HttpListenerResponse response = context.Response;
+
+                StreamReader reader = new StreamReader(requestStream);
+                string responseStr = reader.ReadToEnd();
+
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseStr.ToUpper());
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+            listener.Stop();
+        }
+    }
+}
