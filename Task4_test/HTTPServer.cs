e3cb6d52661640ab0e18b94126c264be1acaa716
Dmitry Lebeshkov <51168295+Lebeshkoff@users.noreply.github.com>
2021-09-17 14:06:30 +0300

    add http server

---
+++ Task4_test/HTTPServer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace Task4_test
+{
+    class HTTPServer
+    {
+        private HttpListener _listener;
+        public event Action<Stream> OnConnect = delegate { };
+        public HTTPServer(string url, int port)
+        {
+            _listener.Prefixes.Add("http://" + url + ":" + port);
+        }
+
+        public Task Start()
+        {
+            while(true)
+            {
+                HttpListenerContext context = _listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                Console.WriteLine(request.HttpMethod + " " + request.UserHostName + " " + request.UserAgent);
+                Stream requestStream = request.InputStream;
+                HttpListenerResponse response = context.Response;
+
+                StreamReader reader = new StreamReader(requestStream);
+                string responseStr = reader.ReadToEnd();
+
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseStr.ToUpper());
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+        }
+    }
+}
---
+++ Task4_test/MathHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using SLAU;
+
+namespace Task4_test
+{
+    class MathHandler
+    {
+        public byte[] GfetMatrix(Stream s)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bf.Serialize(ms, s);
+                return ms.ToArray();
+            }
+        }
+        public double[,] GetMatrix(Stream s)
+        {
+            var sr = new StreamReader(s);
+            var str = sr.ReadToEnd();
+            var bytes = Encoding.UTF8.GetBytes(str);
+            MemoryStream memStream = new MemoryStream();
+            BinaryFormatter binForm = new BinaryFormatter();
+            memStream.Write(bytes, 0, str.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            double[,] matrix = (double[,])binForm.Deserialize(memStream);
+
+            return matrix;
+
+        }
+
+        public double[] SolveMatrix()
+        {
+            return 
+        }
+    }
+}
